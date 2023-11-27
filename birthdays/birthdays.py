45c692366d36e8d3f9c6ab6633a0a8f4c6e3cbc1
icedwater <icedwater@gmail.com>
2023-11-27 22:18:22 +0800

    Added birthdays skeleton and test cases.
    - The code is empty and the tests all fail. It's just to practice TDD.

---
+++ birthdays/birthdays.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+"""
+Grab the birthdays and format them as a note.
+"""
+
+def grab_birthdays():
+    """
+    Connect to a birthday database and retrieve a list of birthdays.
+    Return the list.
+    """
+    pass
+ 
---
+++ birthdays/tests/test_birthdays.py
@@ -0,0 +1,11 @@
+import birthdays
+
+def test_grab_birthdays_no_line():
+    assert False
+
+def test_grab_birthdays_one_line():
+    assert False
+
+def test_grab_birthdays_multi_line():
+    assert False
+
